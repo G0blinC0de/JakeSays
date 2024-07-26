28698169ca0aa6122f15d6a8f0877250dbe1dd9a
G0blinC0de <brewbrotherred@gmail.com>
2024-07-26 15:43:20 -0700

    clean up eventListener, hide startGameHeader on game start

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,8 @@
+const sequence = [];
+let userSequence = [];
+var GameHeader = document.getElementById('startGameHeader');
+
+
 function playAudio(track) {
     const audioPlayer = document.getElementById('audioPlayer');
     audioPlayer.src = 'audio/' + track;
@@ -18,5 +23,25 @@ document.addEventListener('keydown', function (event) {
         case 'Numpad4':
             playAudio('sound4.wav');
             break;
+        case 'Numpad5':
+            playAudio('sound5.wav');
+            break;
+        case 'Numpad6':
+            playAudio('sound6.wav');
+            break;
+        case 'Numpad7':
+            playAudio('sound7.wav');
+            break;
+        case 'Numpad8':
+            playAudio('sound8.wav');
+            break;
+        case ' ':
+            startGame();
+            break;
     }
 });
+
+
+function startGame() {
+    GameHeader.style.display = 'none';
+}
